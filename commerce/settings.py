0010090b446ef5edda8bc84c056bce2ae65b234e
Paul-Bearer <jhdohertyiii@gmail.com>
2026-05-26 17:15:54 -0300

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -133,6 +133,6 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
